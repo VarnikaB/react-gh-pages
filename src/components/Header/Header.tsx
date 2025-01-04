7e5315d3771addc1dccd11c568f4cd24e39e1762
VarnikaB <bagariavarnika@johndeere.com>
2025-01-04 09:11:13 +0530

    separates icon and text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,25 +12,26 @@ const Header = () => (
         <nav id="navbar" className="Navbar navbar nav-menu">
             <ul>
                 <li><a href="#hero" className="nav-link scrollto active">
-                    <span><FaHome style={{width: "3rem"}}/>Home</span></a></li>
-                <li><a href="#about" className="nav-link scrollto"><span><IoMdInformationCircle style={{width: "3rem"}}/>About</span></a>
+                    <span id="Icon-Home"><FaHome style={{width: "3rem"}}/></span><span >Home</span></a></li>
+                <li><a href="#about" className="nav-link scrollto"><span ><IoMdInformationCircle style={{width: "3rem"}}/></span><span >About</span></a>
                 </li>
-                <li><a href="#skills" className="nav-link scrollto"><span><FaGears style={{width: "3rem"}} /> Skills</span></a>
+                <li><a href="#skills" className="nav-link scrollto"><span ><FaGears style={{width: "3rem"}} /></span><span > Skills</span></a>
                 </li>
                 <li><a href="#resume" className="nav-link scrollto">
-                    <span><IoSchoolSharp style={{width: "3rem"}}/>Education</span></a></li>
+                    <span ><IoSchoolSharp style={{width: "3rem"}}/></span><span >Education</span></a></li>
                 <li><a href="#exp" className="nav-link scrollto">
-                    <span><HiBuildingOffice2 style={{width: "3rem"}}/>Experience</span></a></li>
+                    <span ><HiBuildingOffice2 style={{width: "3rem"}}/></span><span >Experience</span></a></li>
                 <li><a href="#portfolio" className="nav-link scrollto">
-                    <span><GrProjects style={{width: "3rem"}}/>Projects</span></a></li>
-                <li><a href="#extra" className="nav-link scrollto"><span><BiCommand style={{width: "3rem"}}/>Hobbies</span></a>
+                    <span ><GrProjects style={{width: "3rem"}}/></span><span >Projects</span></a></li>
+                <li><a href="#extra" className="nav-link scrollto"><span ><BiCommand style={{width: "3rem"}}/></span><span >Hobbies</span></a>
                 </li>
-                <li><a href="#pp" className="nav-link scrollto"><span><FaBook style={{width: "3rem"}}/>Paper Publication</span></a>
+                <li><a href="#pp" className="nav-link scrollto"><span ><FaBook style={{width: "3rem"}}/></span><span >Paper Publication</span></a>
                 </li>
-                <li><a href="#contact" className="nav-link scrollto"><i className="bi bi-person-lines-fill"></i>
-                    <span><FaPhone style={{width: "3rem"}}/>Contact</span></a></li>
+                <li><a href="#contact" className="nav-link scrollto"><span ><FaPhone style={{width: "3rem"}}/></span>
+                    <span >Contact</span></a></li>
             </ul>
         </nav>
 )
+
 
 export default Header;
